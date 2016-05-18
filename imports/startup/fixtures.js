d44b4fdb22e0995a14aba9034091b340757d367f
Gustavo Kluwe Saudade <gustavo.saudade@cwinet.local>
2016-05-18 16:13:19 -0300

    fix urlImages to pacotesTop

--- imports/startup/fixtures.js
+++ imports/startup/fixtures.js
@@ -5,7 +5,7 @@ import { Rotas } from '../api/rotas';
 
 Meteor.startup(() => {
   if (Rotas.find().count() === 0) {
-    HTTP.call( 'GET', 'http://www.mocky.io/v2/573cabab110000c321aa8b1d', {"Content-Type": "application/json"}, function( error, response ) {
+    HTTP.call( 'GET', 'http://www.mocky.io/v2/573cbe2b1100003e24aa8b41', {"Content-Type": "application/json"}, function( error, response ) {
       if ( error ) {
         console.log( error );
       } else {
